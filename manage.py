9b40df34ee0c2d9aac8812c0e598220a35627a47
onlinejudge95 <onlinejudge95@gmail.com>
2020-08-29 18:18:23 +0530

    Fixes failing lint tests

--- manage.py
+++ manage.py
@@ -4,5 +4,8 @@ import os
 from app import factory
 
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s PID- %(process)d %(levelname)s %(pathname)s %(funcName)s %(lineno)d %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s PID- %(process)d %(levelname)s %(pathname)s %(funcName)s %(lineno)d %(message)s",
+)
 app = factory.create_app(os.getenv("FLASK_ENV"))
