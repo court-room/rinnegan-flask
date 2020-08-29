cdfd04285dd66b1bec4fed02ecb00d6b0d09306d
onlinejudge95 <onlinejudge95@gmail.com>
2020-08-29 18:24:20 +0530

    Updates black issue

--- manage.py
+++ manage.py
@@ -6,8 +6,5 @@ from app import factory
 
 log_format_string = "%(asctime)s PID- %(process)d %(levelname)s %(pathname)s %(funcName)s %(lineno)d %(message)s"  # noqa: E501
 
-logging.basicConfig(
-    level=logging.INFO,
-    format=log_format_string,
-)
+logging.basicConfig(level=logging.INFO, format=log_format_string)
 app = factory.create_app(os.getenv("FLASK_ENV"))
