4c1dda1b70aa7d30e34728bda0aa45eae28df994
onlinejudge95 <onlinejudge95@gmail.com>
2020-08-29 18:22:53 +0530

    Fixes flake8 issue

--- manage.py
+++ manage.py
@@ -4,8 +4,10 @@ import os
 from app import factory
 
 
+log_format_string = "%(asctime)s PID- %(process)d %(levelname)s %(pathname)s %(funcName)s %(lineno)d %(message)s"  # noqa: E501
+
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s PID- %(process)d %(levelname)s %(pathname)s %(funcName)s %(lineno)d %(message)s",
+    format=log_format_string,
 )
 app = factory.create_app(os.getenv("FLASK_ENV"))
