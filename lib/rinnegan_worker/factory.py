a0c74ac79c223706b9293ab8ed6f2e768669a5f1
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-01 15:20:05 +0530

    Fixes deepsource issue

--- lib/rinnegan_worker/factory.py
+++ lib/rinnegan_worker/factory.py
@@ -7,31 +7,31 @@ from lib.rinnegan_worker.vendors import client_map as vendor_client_map
 
 class BaseFactory(abc.ABC):
     @abc.abstractstaticmethod
-    def build_client(source):
+    def build_client(client_type):
         pass
 
 
 class SourceClientFactory(BaseFactory):
     @staticmethod
-    def build_client(source):
-        client = source_client_map.get(source, None)
+    def build_client(client_type):
+        client = source_client_map.get(client_type, None)
         if not client:
             error = f"""
-            Following source:- {source} is not integrated with rinnegan
+            Following source:- {client_type} is not integrated with rinnegan
             """
             raise NotImplementedError(error)
 
-        return client(config_map[source]())
+        return client(config_map[client_type]())
 
 
 class StorageVendorClientFactory(BaseFactory):
     @staticmethod
-    def build_client(vendor):
-        client = vendor_client_map.get(vendor, None)
+    def build_client(client_type):
+        client = vendor_client_map.get(client_type, None)
         if not client:
             error = f"""
-            Following vendor:- {vendor} is not integrated with rinnegan
+            Following vendor:- {client_type} is not integrated with rinnegan
             """
             raise NotImplementedError(error)
 
-        return client(config_map[vendor]())
+        return client(config_map[client_type]())
