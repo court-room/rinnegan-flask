379dfa69e1312c75736237a8cec9d9f6d945f67b
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-05 17:32:59 +0530

    Removes deepsource issue

--- lib/rinnegan_worker/config.py
+++ lib/rinnegan_worker/config.py
@@ -1,6 +1,26 @@
 import os
 
 
+def read_secrets(secret_file_path):
+        """
+        Utility method to fetch secrets depending upon the environment.
+        First an attempt is made to read the secret from the file
+        defined in the env variable.
+        If the file does not exist then the value read is returned.
+
+        :param: secret_file_path
+            Path to file or the secret
+        :returns:
+            Secret stored in the environment or the file
+        """
+        try:
+            with open(secret_file_path, "r") as fp:
+                secret = fp.readline().strip()
+            return secret
+        except (FileNotFoundError, TypeError):
+            return os.getenv(secret_file_path)
+
+
 class BaseConfig:
     @staticmethod
     def read_secrets(secret_file_path):
@@ -23,9 +43,9 @@ class BaseConfig:
             return os.getenv(secret_file_path)
 
 
-class TwitterConfig(BaseConfig):
-    TWITTER_CONSUMER_KEY = BaseConfig.read_secrets("TWITTER_CONSUMER_KEY_FILE")
-    TWITTER_CONSUMER_SECRET = BaseConfig.read_secrets(
+class TwitterConfig:
+    TWITTER_CONSUMER_KEY = read_secrets("TWITTER_CONSUMER_KEY_FILE")
+    TWITTER_CONSUMER_SECRET = read_secrets(
         "TWITTER_CONSUMER_SECRET_FILE"
     )
 
