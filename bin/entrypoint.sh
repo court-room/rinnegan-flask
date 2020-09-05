592093f937ec8812795078c7dfb126b640d70729
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-06 02:25:41 +0530

    Common interface for reading secrets

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -7,12 +7,17 @@ if [ $NODE_TYPE == "server" ]; then
     
     if [ $FLASK_ENV != "development" ]; then
         echo "Running Gunicorn with eventlet workers"
-        gunicorn --config /usr/src/app/gunicorn.conf.py manage:app
+        
+        export NEW_RELIC_LICENSE_KEY=`cat $NEW_RELIC_LICENSE_KEY_FILE`
+        export NEW_RELIC_APP_NAME=`cat $NEW_RELIC_APP_NAME_FILE`
+        
+        newrelic-admin run-program gunicorn --config gunicorn.conf.py manage:app
     else
         echo "Running the single threaded flask server"
         flask run --host 0.0.0.0
     fi
 else
     mkdir -p /usr/src/app/data/worker-data
-    rq worker --url redis://:rinnegan@redis:6379/0 rinnegan
+    REDIS_URL=`cat $REDIS_URL_FILE`
+    rq worker --url $REDIS_URL rinnegan
 fi
