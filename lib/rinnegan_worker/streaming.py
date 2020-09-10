b5b6f70328eee0c9a31572ad16630a5ee685952d
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-09-11 00:30:08 +0530

    Starts indexing (#44)
    * Starts indexing
    
    * temp
    
    * Data is getting stored in mongo db
    
    * Updates config
    
    * Resolves lint issues
    
    * Fixes deepsource issues

--- lib/rinnegan_worker/streaming.py
+++ lib/rinnegan_worker/streaming.py
@@ -6,20 +6,35 @@ from pymongo import MongoClient
 class BaseClient(abc.ABC):
     def __init__(self, config_obj):
         self.config = config_obj
+        self.data = None
 
     @abc.abstractmethod
-    def start_streaming(self, keyword, response):
+    def start_streaming(self, keyword, responses):
         pass
 
 
 class MongoDBClient(BaseClient):
     def __init__(self, config_obj):
         super().__init__(config_obj)
+        self.client = MongoClient(
+            self.config.MONGO_URI, authSource=self.config.MONGO_AUTH_SOURCE
+        )
+        self.db = self.client[self.config.MONGO_DATABASE]
+        self.collection = self.db[self.config.MONGO_MODEL_COLLECTION]
 
-        self.client = MongoClient(self.config.MONGO_URI)
+    def start_streaming(self, keyword, responses):
+        self.data = []
 
-    def start_streaming(self, keyword, response):
-        pass
+        for response in responses:
+            temp = response
+            temp["text"] = temp["text"].encode("ascii", "ignore").decode()
+            for obj in temp["classifications"]:
+                del obj["tag_id"]
+            del temp["external_id"]
+            self.data.append(temp)
+
+        self.collection.insert_many(self.data)
+        self.data = None
 
 
 client_map = {"mongo": MongoDBClient}
