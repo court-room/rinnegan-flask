a0498df73ac0f51c08be300dab9f8d5be87f8df4
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-19 11:03:26 +0530

    Adds redis vars in entrypoint.sh

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -9,10 +9,13 @@ if [ $NODE_TYPE == "server" ]; then
         echo "Running Gunicorn with eventlet workers"
         gunicorn --bind 0.0.0.0:5000 --worker-class eventlet manage:app
     else
-        echo "Running Gunicorn with sync workers"
-        gunicorn --bind 0.0.0.0:5000 manage:app
+        echo "Running Flask single threaded worker"
+        # gunicorn --bind 0.0.0.0:5000 manage:app
+        flask run --host "0.0.0.0" --port 5000
     fi
 else
     mkdir -p /usr/src/app/data/worker-data
-    rq worker --url redis://:rinnegan@redis:6379/0 rinnegan
+    REDIS_URL=`cat "${REDIS_URL_FILE}"`
+    REDIS_QUEUE=`cat "${REDIS_QUEUE_FILE}"`
+    rq worker --url $REDIS_URL $REDIS_QUEUE
 fi
