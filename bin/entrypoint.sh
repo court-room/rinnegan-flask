b147df766e951a3d7eeaa737f59c328eb5321f65
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-28 21:46:47 +0530

    Adds ssl support in local

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,13 +4,15 @@
 if [ $NODE_TYPE == "server" ]; then
     
     flask db upgrade
+    SSL_CERT_FILE=`cat "${SSL_CERT_FILE}"`
+    SSL_KEY_FILE=`cat "${SSL_KEY_FILE}"`
     
     if [ $FLASK_ENV != "development" ]; then
         echo "Running Flask single threaded worker"
-        newrelic-admin run-program flask run --host "0.0.0.0" --port 5000
+        newrelic-admin run-program flask run --host "0.0.0.0" --port 5000 --cert $SSL_CERT_FILE --key $SSL_KEY_FILE
     else
         echo "Running Flask single threaded worker"
-        flask run --host "0.0.0.0" --port 5000
+        flask run --host "0.0.0.0" --port 5000 --cert $SSL_CERT_FILE --key $SSL_KEY_FILE
     fi
 else
     mkdir -p /usr/src/app/data/worker-data
